3c14900689b26a2bb6e826eaf5de380574770c79
flovidraw <120114787+flovidraw@users.noreply.github.com>
2023-04-17 15:58:41 +0200

    Create main.dart

---
+++ bibliothèque/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.microphone.request();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
